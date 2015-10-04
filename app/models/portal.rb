ffe8127f70907f8c417a67478d32511a6cdf5937
Phil Battos <philbattos@gmail.com>
2015-10-03 18:08:43 -0600

    Remove research url from tunnel parameters

--- app/models/portal.rb
+++ app/models/portal.rb
@@ -3,8 +3,8 @@ class Portal
   # responsible for connecting research.tsadra.org with librarywiki.tsadra.org
   # example query  = https://research.tsadra.org/index.php?title=Special%3AAsk&q=%5B%5BBuddhism%2C+Sexuality%2C+and+Gender%5D%5D&po=author%0D%0Afulltitle%0D%0Apublisher%0D%0A&eq=yes&p%5Bformat%5D=csv&p%5Bsep%5D=%2C
   def fetch(query)
-    research_wiki   = 'https://research.tsadra.org/index.php?'
-    research_query  = research_wiki + query
+    root_url        = 'https://research.tsadra.org/index.php?'
+    research_query  = root_url + query
     uri             = build_uri(research_query)
     response        = authenticate_request(uri)
   end
@@ -12,7 +12,9 @@ class Portal
   # bypasses the DRL firewall to provide access to research.tsadra.org
   # example request: https://tsadra.herokuapp.com/tunnel?https://research.tsadra.org/images/1/1b/The_Evolution_of_the_Buddha_Image-front.jpeg
   def bypass_firewall(query)
-    uri       = build_uri(query)
+    root_url  = 'https://research.tsadra.org/'
+    full_url  = root_url + query
+    uri       = build_uri(full_url)
     response  = authenticate_request(uri)
   end
 
